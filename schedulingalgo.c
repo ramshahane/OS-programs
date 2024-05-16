a2b8c6b5d3fed895829052ab87820da2abc313cc
Ram Shahane <147724694+ramshahane@users.noreply.github.com>
2024-05-16 22:44:53 +0530

    Update schedulingalgo.c

--- schedulingalgo.c
+++ schedulingalgo.c
@@ -1,261 +1,275 @@
-/*
 
-Problem Statement -:Write a C program to compute and print the average waiting time, 
-average turnaround time and CPU burst times for the given list of 
-processes. Display/print the Gantt chart for first come first serve, shortest 
-job first, priority scheduling and round robin scheduling algorithm. 
+
+/* Problem Statement: Lab 3
+Write a C program to compute and print the average waiting time,
+average turnaround time and CPU burst times for the given list of
+processes. Display/print the Gantt chart for first come first serve, shortest
+job first, priority scheduling and round robin scheduling algorithm.
 */
-#include<stdio.h>
 
-float calculateAvgWaitingTime(int processes[], int n, int burstTime[], int waitingTime[])
-{
-    int i;
-    float avgWaitingTime = 0;
+#include <stdio.h>
 
-   
-    waitingTime[0] = 0;
+#define MAX_PROCESSES 10
 
-    for(i = 1; i < n; i++)
-    {
-        waitingTime[i] = burstTime[i-1] + waitingTime[i-1];
-        avgWaitingTime += waitingTime[i];
+typedef struct {
+    int pid;
+    int arrival_time;
+    int burst_time;
+    int priority;
+} Process;
+
+void fcfs(Process p[], int n) {
+    int wt[n], tat[n], total_wt = 0, total_tat = 0;
+    int bt[n];
+    for (int i = 0; i < n; i++) {
+        bt[i] = p[i].burst_time;
     }
 
-    avgWaitingTime /= n; 
+    wt[0] = 0;
+    for (int i = 1; i < n; i++) {
+        wt[i] = bt[i - 1] + wt[i - 1];
+    }
 
-    return avgWaitingTime;
+    for (int i = 0; i < n; i++) {
+        tat[i] = bt[i] + wt[i];
+        total_wt += wt[i];
+        total_tat += tat[i];
+    }
+
+    printf("FCFS Scheduling:\n");
+    printf("Process\tBurst Time\tWaiting Time\tTurnaround Time\n");
+    for (int i = 0; i < n; i++) {
+        printf("P%d\t%d\t\t%d\t\t%d\n", p[i].pid, bt[i], wt[i], tat[i]);
+    }
+    printf("Average Waiting Time: %.2f\n", (float)total_wt / n);
+    printf("Average Turnaround Time: %.2f\n", (float)total_tat / n);
+
+    // Gantt Chart
+    printf("Gantt Chart:\n");
+    for (int i = 0; i < n; i++) {
+        printf("P%d |", p[i].pid);
+    }
+    printf("\n");
 }
 
-
-float calculateAvgTurnaroundTime(int processes[], int n, int burstTime[], int waitingTime[], int turnaroundTime[])
-{
-    int i;
-    float avgTurnaroundTime = 0;
-
-    for(i = 0; i < n; i++)
-    {
-        turnaroundTime[i] = burstTime[i] + waitingTime[i];
-        avgTurnaroundTime += turnaroundTime[i];
+void sjf(Process p[], int n) {
+    int wt[n], tat[n], total_wt = 0, total_tat = 0;
+    int bt[n];
+    for (int i = 0; i < n; i++) {
+        bt[i] = p[i].burst_time;
     }
 
-    avgTurnaroundTime /= n; 
+    // Sort processes by burst time
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (bt[i] > bt[j]) {
+                int temp = bt[i];
+                bt[i] = bt[j];
+                bt[j] = temp;
 
-    return avgTurnaroundTime;
-}
-
-void displayGanttChart(int processes[], int n, int burstTime[], int choice)
-{
-    int i;
-    char process_char = 'A';
-
-    printf("\nGantt Chart:\n\n");
-    printf(" ");
-    for(i = 0; i < n; i++)
-    {
-        printf("-------");
-    }
-    printf("\n|");
-
-    for(i = 0; i < n; i++)
-    {
-        printf("  P%c  |", process_char++);
-    }
-    printf("\n ");
-    for(i = 0; i < n; i++)
-    {
-        printf("-------");
-    }
-
-    printf("\n0");
-    for(i = 0; i < n; i++)
-    {
-        printf("      %d", burstTime[i]);
-    }
-    printf("\n\n");
-
-    if(choice == 1)
-    {
-        printf("FCFS Scheduling Gantt Chart\n");
-    }
-    else if(choice == 2)
-    {
-        printf("SJF Scheduling Gantt Chart\n");
-    }
-}
-
-void sortBurstTime(int processes[], int n, int burstTime[])
-{
-    int i, j, minIndex, temp;
-
-    for(i = 0; i < n-1; i++)
-    {
-        minIndex = i;
-
-        for(j = i+1; j < n; j++)
-        {
-            if(burstTime[j] < burstTime[minIndex])
-            {
-                minIndex = j;
+                temp = p[i].pid;
+                p[i].pid = p[j].pid;
+                p[j].pid = temp;
             }
         }
-
-        temp = burstTime[minIndex];
-        burstTime[minIndex] = burstTime[i];
-        burstTime[i] = temp;
-
-        temp = processes[minIndex];
-        processes[minIndex] = processes[i];
-        processes[i] = temp;
     }
+
+    wt[0] = 0;
+    for (int i = 1; i < n; i++) {
+        wt[i] = bt[i - 1] + wt[i - 1];
+    }
+
+    for (int i = 0; i < n; i++) {
+        tat[i] = bt[i] + wt[i];
+        total_wt += wt[i];
+        total_tat += tat[i];
+    }
+
+    printf("SJF Scheduling:\n");
+    printf("Process\tBurst Time\tWaiting Time\tTurnaround Time\n");
+    for (int i = 0; i < n; i++) {
+        printf("P%d\t%d\t\t%d\t\t%d\n", p[i].pid, bt[i], wt[i], tat[i]);
+    }
+    printf("Average Waiting Time: %.2f\n", (float)total_wt / n);
+    printf("Average Turnaround Time: %.2f\n", (float)total_tat / n);
+
+    // Gantt Chart
+    printf("Gantt Chart:\n");
+    for (int i = 0; i < n; i++) {
+        printf("P%d |", p[i].pid);
+    }
+    printf("\n");
 }
 
-void sortPriority(int processes[], int n, int burstTime[], int priority[])
-{
-    int i, j, minIndex, temp;
+void priority(Process p[], int n) {
+    int wt[n], tat[n], total_wt = 0, total_tat = 0;
+    int bt[n];
+    for (int i = 0; i < n; i++) {
+        bt[i] = p[i].burst_time;
+    }
 
-    for(i = 0; i < n-1; i++)
-    {
-        minIndex = i;
-
-        for(j = i+1; j < n; j++)
-        {
-            if(priority[j] < priority[minIndex])
-            {
-                minIndex = j;
+    // Sort processes by priority
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (p[i].priority < p[j].priority) {
+                Process temp = p[i];
+                p[i] = p[j];
+                p[j] = temp;
             }
         }
-
-        temp = burstTime[minIndex];
-        burstTime[minIndex] = burstTime[i];
-        burstTime[i] = temp;
-
-        temp = priority[minIndex];
-        priority[minIndex] = priority[i];
-        priority[i] = temp;
-
-        temp = processes[minIndex];
-        processes[minIndex] = processes[i];
-        processes[i] = temp;
     }
+
+    wt[0] = 0;
+    for (int i = 1; i < n; i++) {
+        wt[i] = bt[i - 1] + wt[i - 1];
+    }
+
+    for (int i = 0; i < n; i++) {
+        tat[i] = bt[i] + wt[i];
+        total_wt += wt[i];
+        total_tat += tat[i];
+    }
+
+    printf("Priority Scheduling:\n");
+    printf("Process\tBurst Time\tPriority\tWaiting Time\tTurnaround Time\n");
+    for (int i = 0; i < n; i++) {
+        printf("P%d\t%d\t\t%d\t\t%d\t\t%d\n", p[i].pid, bt[i], p[i].priority, wt[i], tat[i]);
+    }
+    printf("Average Waiting Time: %.2f\n", (float)total_wt / n);
+    printf("Average Turnaround Time: %.2f\n", (float)total_tat / n);
+
+    // Gantt Chart
+    printf("Gantt Chart:\n");
+    for (int i = 0; i < n; i++) {
+        printf("P%d |", p[i].pid);
+    }
+    printf("\n");
 }
 
-void roundRobinScheduling(int processes[], int n, int burstTime[], int quantum)
-{
-    int i, remainingTime[n], totalWaitingTime = 0, totalTime = 0;
-
-    for(i = 0; i < n; i++)
-    {
-        remainingTime[i] = burstTime[i];
+void round_robin(Process p[], int n, int quantum) {
+    int wt[n], tat[n], total_wt = 0, total_tat = 0;
+    int bt[n];
+    for (int i = 0; i < n; i++) {
+        bt[i] = p[i].burst_time;
     }
 
-    while(1)
-    {
+    int time = 0;
+    int remaining[n];
+    for (int i = 0; i < n; i++) {
+        remaining[i] = bt[i];
+    }
+
+    while (1) {
         int done = 1;
-
-        for(i = 0; i < n; i++)
-        {
-            if(remainingTime[i] > 0)
-            {
-                done = 0; 
-
-                if(remainingTime[i] > quantum)
-                {
-                    totalTime += quantum;
-                    remainingTime[i] -= quantum;
+        for (int i = 0; i < n; i++) {
+            if (remaining[i] > 0) {
+                done = 0;
+                if (remaining[i] > quantum) {
+                    time += quantum;
+                    remaining[i] -= quantum;
+                } else {
+                    time += remaining[i];
+                    remaining[i] = 0;
                 }
-                else
-                {
-                    totalTime += remainingTime[i];
-                    totalWaitingTime += totalTime - burstTime[i];
-                    remainingTime[i] = 0;
-                }
+                printf("P%d |", p[i].pid);
             }
         }
+        if (done) break;
+    }
+    printf("\n");
 
-        if(done == 1)
-            break;
+    wt[0] = 0;
+    for (int i = 1; i < n; i++) {
+        wt[i] = wt[i - 1] + bt[i - 1];
     }
 
-    printf("\nAverage Waiting Time: %.2f", (float)totalWaitingTime/n);
-    printf("\nAverage Turnaround Time: %.2f", (float)totalTime/n);
+    for (int i = 0; i < n; i++) {
+        tat[i] = bt[i] + wt[i];
+        total_wt += wt[i];
+        total_tat += tat[i];
+    }
+
+    printf("Round Robin Scheduling (Quantum = %d):\n", quantum);
+    printf("Process\tBurst Time\tWaiting Time\tTurnaround Time\n");
+    for (int i = 0; i < n; i++) {
+        printf("P%d\t%d\t\t%d\t\t%d\n", p[i].pid, bt[i], wt[i], tat[i]);
+    }
+    printf("Average Waiting Time: %.2f\n", (float)total_wt / n);
+    printf("Average Turnaround Time: %.2f\n", (float)total_tat / n);
 }
 
-int main()
-{
-    int n, i, choice, quantum;
-    char repeat;
+int main() {
+    Process p[] = {
+        {1, 0, 10, 3},
+        {2, 1, 5, 1},
+        {3, 2, 8, 2},
+        {4, 3, 12, 4},
+        {5, 4, 4, 5}
+    };
+    int n = sizeof(p) / sizeof(p[0]);
 
-    do {
-        printf("\nEnter the number of processes: ");
-        scanf("%d", &n);
+    fcfs(p, n);
+    printf("\n");
 
-        int processes[n];
-        int burstTime[n];
-        int waitingTime[n];
-        int turnaroundTime[n];
-        int priority[n];
+    sjf(p, n);
+    printf("\n");
 
-        for(i = 0; i < n; i++)
-        {
-            printf("Enter the burst time for process P%d: ", i + 1);
-            scanf("%d", &burstTime[i]);
-            processes[i] = i + 1; 
-        }
+    priority(p, n);
+    printf("\n");
 
-        do {
-            printf("\nChoose a scheduling algorithm:\n");
-            printf("1. First Come First Serve (FCFS)\n");
-            printf("2. Shortest Job First (SJF)\n");
-            printf("3. Priority Scheduling\n");
-            printf("4. Round Robin Scheduling\n");
-            printf("Enter your choice: ");
-            scanf("%d", &choice);
-
-            switch(choice)
-            {
-                case 1:
-                    displayGanttChart(processes, n, burstTime, choice);
-                    printf("\nAverage Waiting Time: %.2f", calculateAvgWaitingTime(processes, n, burstTime, waitingTime));
-                    printf("\nAverage Turnaround Time: %.2f", calculateAvgTurnaroundTime(processes, n, burstTime, waitingTime, turnaroundTime));
-                    break;
-
-                case 2:
-                    sortBurstTime(processes, n, burstTime);
-                    displayGanttChart(processes, n, burstTime, choice);
-                    printf("\nAverage Waiting Time: %.2f", calculateAvgWaitingTime(processes, n, burstTime, waitingTime));
-                    printf("\nAverage Turnaround Time: %.2f", calculateAvgTurnaroundTime(processes, n, burstTime, waitingTime, turnaroundTime));
-                    break;
-
-                case 3:
-                    for(i = 0; i < n; i++)
-                    {
-                        printf("Enter the priority for process P%d: ", i + 1); // Start from P1
-                        scanf("%d", &priority[i]);
-                    }
-
-                    sortPriority(processes, n, burstTime, priority);
-                    displayGanttChart(processes, n, burstTime, choice);
-                    printf("\nAverage Waiting Time: %.2f", calculateAvgWaitingTime(processes, n, burstTime, waitingTime));
-                    printf("\nAverage Turnaround Time: %.2f", calculateAvgTurnaroundTime(processes, n, burstTime, waitingTime, turnaroundTime));
-                    break;
-
-                case 4:
-                    printf("Enter the time quantum: ");
-                    scanf("%d", &quantum);
-                    roundRobinScheduling(processes, n, burstTime, quantum);
-                    break;
-
-                default:
-                    printf("Invalid choice. Please try again.\n");
-            }
-
-            printf("\nDo you want to repeat? (Y/N): ");
-            scanf(" %c", &repeat);
-        } while(repeat == 'Y' || repeat == 'y');
-
-        printf("\nDo you want to enter new processes? (Y/N): ");
-        scanf(" %c", &repeat);
-    } while(repeat == 'Y' || repeat == 'y');
+    round_robin(p, n, 2);
+    printf("\n");
 
     return 0;
 }
+
+/*
+Test Case: 
+~/OS-Lab$ ./lab-3
+FCFS Scheduling:
+Process Burst Time  Waiting Time    Turnaround Time
+P1  10      0       10
+P2  5       10      15
+P3  8       15      23
+P4  12      23      35
+P5  4       35      39
+Average Waiting Time: 16.60
+Average Turnaround Time: 24.40
+Gantt Chart:
+P1 |P2 |P3 |P4 |P5 |
+
+SJF Scheduling:
+Process Burst Time  Waiting Time    Turnaround Time
+P5  4       0       4
+P2  5       4       9
+P3  8       9       17
+P1  10      17      27
+P4  12      27      39
+Average Waiting Time: 11.40
+Average Turnaround Time: 19.20
+Gantt Chart:
+P5 |P2 |P3 |P1 |P4 |
+
+Priority Scheduling:
+Process Burst Time  Priority    Waiting Time    Turnaround Time
+P4  10      5       0       10
+P1  5       4       10      15
+P5  8       3       15      23
+P3  12      2       23      35
+P2  4       1       35      39
+Average Waiting Time: 16.60
+Average Turnaround Time: 24.40
+Gantt Chart:
+P4 |P1 |P5 |P3 |P2 |
+
+P4 |P1 |P5 |P3 |P2 |P4 |P1 |P5 |P3 |P2 |P1 |P5 |P3 |P2 |P1 |P5 |P3 |P1 |P5 |P1 |
+Round Robin Scheduling (Quantum = 2):
+Process Burst Time  Waiting Time    Turnaround Time
+P4  4       0       4
+P1  12      4       16
+P5  10      16      26
+P3  8       26      34
+P2  5       34      39
+Average Waiting Time: 16.00
+Average Turnaround Time: 23.80
+*/
